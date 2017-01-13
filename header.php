dfac19d143b476c76c701fe33af6e43b4205e9e4
Jen Downs <hello@jendowns.com>
2017-01-12 23:09:05 -0600

    Updated theme, link, & url info in header

--- header.php
+++ header.php
@@ -1,41 +1,41 @@
 <?php
 /**
  * @package WordPress
- * @subpackage CSD
- * @since CSD 1.0
+ * @subpackage good-dogs
+ * @since good-dogs 1.0
  */
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Luna and Ash: Two Awesome Dogs</title>
+	<title>Two Good Dogs</title>
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
 	<meta charset="utf-8"/>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 
 	<meta name="author" content="Jen Downs">
-	<meta name="description" content="Luna the Aussie Shepherd and Ash the Corgi are the two most awesome dogs you could ever meet."/>
+	<meta name="description" content="Luna the Aussie Shepherd and Ash the Corgi are the two very good dogs."/>
 	<meta name="keywords" content="dog, dogs, puppy, puppies, best friends, awesome, pet, pets, corgi, pembroke welsh corgi, australian shepherd" />
 	<meta name="robots" content="index,follow" />
 
 	<!-- Facebook Sharing Tags -->
-	<meta property="og:url" content="http://jen.li/csd/">
+	<meta property="og:url" content="http://jen.li/good-dogs/">
 	<meta property="og:author" content="Jen Downs">
 	<meta property="og:type" content="profile">
-	<meta property="og:title" content="The Two Most Awesome Dogs">
+	<meta property="og:title" content="Two Good Dogs">
 	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/cover.jpg">
 	<meta property="og:image:width" content="500">
 	<meta property="og:image:height" content="362">
-	<meta property="og:description" content="Luna the Australian Shepherd and Ash the Corgi are the two most awesome dogs you could ever meet. Check out all the goofy things they get into!">
+	<meta property="og:description" content="Luna the Australian Shepherd and Ash the Corgi are two very good dogs.">
 
 	<!-- Twitter Card -->
 	<meta name="twitter:card" content="summary" />
 	<meta property="twitter:creator" content="@jenisora">
-	<meta property="twitter:title" content="The Two Most Awesome Dogs">
+	<meta property="twitter:title" content="Two Good Dogs">
 	<meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/cover.jpg">
 	<meta property="twitter:image:alt" content="Luna and Ash sitting next to each other.">
-	<meta property="twitter:description" content="Luna the Aussie Shepherd and Ash the Corgi are the two most awesome dogs you could ever meet. Check out all the goofy things they get into!">
+	<meta property="twitter:description" content="Luna the Aussie Shepherd and Ash the Corgi are two good dogs.">
 
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" type="image/x-icon"/>
 
